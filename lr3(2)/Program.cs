ae87ffbd998caafe5442a9aeb5714d255cab3b4d
VikorMerzlyakov <111169794+VikorMerzlyakov@users.noreply.github.com>
2025-10-04 15:55:00 +0300

    LR3(2)

---
+++ lr3(2)/Program.cs
@@ -0,0 +1,27 @@
+﻿for (int i = 1; i < 11; i++)
+{
+    Client reader = new Client(i);
+}
+class Client
+{
+    static Semaphore sem = new Semaphore(3, 3);
+    Thread myThread;
+    int count = 3;
+
+    public Client(int i)
+    {
+        myThread = new Thread(EnterClub);
+        myThread.Name = $"Посетитель {i}";
+        myThread.Start();
+    }
+
+    public void EnterClub()
+    {
+        sem.WaitOne();
+        Console.WriteLine($"{Thread.CurrentThread.Name} входит в клуб");
+        Thread.Sleep(1000);
+        Console.WriteLine($"{Thread.CurrentThread.Name} вышел из клуба.");
+        sem.Release();
+
+    }
+}
