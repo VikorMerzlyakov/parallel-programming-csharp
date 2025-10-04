c7d719d72c4849a18f2c209b86cef93b39b7c23b
VikorMerzlyakov <111169794+VikorMerzlyakov@users.noreply.github.com>
2025-10-04 15:41:57 +0300

    LR3(1)

---
+++ lr3(1)/Program.cs
@@ -0,0 +1,27 @@
+﻿for (int i = 1; i < 11; i++)
+{
+    Sleeper reader = new Sleeper(i);
+}
+class Sleeper
+{
+    static Semaphore sem = new Semaphore(3, 3);
+    Thread myThread;
+    int count = 3;
+
+    public Sleeper(int i)
+    {
+        myThread = new Thread(Sleep);
+        myThread.Name = $"Поток {i}";
+        myThread.Start();
+    }
+
+    public void Sleep()
+    {
+            sem.WaitOne();
+            Console.WriteLine($"{Thread.CurrentThread.Name} засыпает");
+            Thread.Sleep(1000);
+            Console.WriteLine($"{Thread.CurrentThread.Name} проснулся.");
+            sem.Release();
+
+    }
+}
