a96cb5b8dc7cba37a92864a7cde4deb0aefafc65
VikorMerzlyakov <111169794+VikorMerzlyakov@users.noreply.github.com>
2025-10-04 16:15:08 +0300

    LR3

---
+++ lr3/lr3(3)/Program.cs
@@ -0,0 +1,31 @@
+﻿for (int i = 1; i < 11; i++)
+{
+    Sleeper reader = new Sleeper(i);
+}
+class Sleeper
+{
+    static Semaphore sem = new Semaphore(10, 10);
+    Thread myThread;
+    int count = 3;
+
+    public Sleeper(int i)
+    {
+        myThread = new Thread(ParkCar);
+        myThread.Name = $"Поток {i}";
+        myThread.Start();
+    }
+
+    public void ParkCar()
+    {
+        for (int day = 0; day < 3; day++)
+        {
+            sem.WaitOne();
+            Console.WriteLine($"{Thread.CurrentThread.Name} припарковался.");
+            Thread.Sleep(1000);
+            Console.WriteLine($"{Thread.CurrentThread.Name} выехал.");
+            sem.Release();
+            Thread.Sleep(500);
+        }
+
+    }
+}
